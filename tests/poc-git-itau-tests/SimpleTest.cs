ea61265c53fa7771d5d69c36ac11661cdbece5f4
Bruno Joaquim <bruno.joaquim@eximia.co>
2023-06-26 21:32:42 -0300

    First commit

---
+++ tests/poc-git-itau-tests/SimpleTest.cs
@@ -0,0 +1,11 @@
+namespace Poc.Git.Itau_tests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
